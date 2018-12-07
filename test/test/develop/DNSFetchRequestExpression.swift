402ed06a930d82341c93d78161fba466d0bab938
yokota-san <yokota-san@users.noreply.github.com>
2018-12-08 00:32:25 +0900

    no message

---
+++ test/test/develop/DNSFetchRequestExpression.swift
@@ -0,0 +1,13 @@
+//
+//  DNSFetchRequestExpression.swift
+//  test
+//
+//  Created by yokotasan on 2018/12/08.
+//  Copyright © 2018年 yokotasan. All rights reserved.
+//
+
+import UIKit
+
+class DNSFetchRequestExpression: NSFetchRequestExpression {
+
+}
